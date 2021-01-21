bad7e6d3beb0d5a94c499fb0d11c5b816fa69215
SCN <supercodingninja@outlook.com>
2021-01-20 23:33:19 -0800

    Created fileGenerator.js

---
+++ fileGenerator.js
@@ -0,0 +1,101 @@
+var fileGenerator = require("./fileGenerator");
+var fs = require("fs")
+var inquirer = require('inquirer');
+
+// questions to ask the user //
+Const questions = [
+	{
+        type: "input",
+		message: “What is the title of your repository?”
+        name: "title",
+    },{
+        type: "input",
+		message: “Please give your logo information.”
+        name: "logo",
+    },{
+        type: "input",
+		message: “What is your GitHub user name?”
+        name: "userName",
+    },{
+        type: "input",
+		message: “Please give your GitHub profile link.”
+        name: "GitHub",
+    },{
+        type: "input",
+		message: “What is your email?”
+        name: "email",
+    },{
+        type: "list",
+		name: “license”
+        message: "Please select which license you would like to use."
+        	: [{
+            "APACHE 2.O",
+            "BSD 3",
+            "GVL-GPL 3.0",
+            "MIT",
+            "None"
+        }]
+    },{
+        type: "input",
+        name: "description",
+        message: "Please “describe the repository.
+
+    },{
+        type: "input",
+		message: “Please state if others can contribute.”
+        name: "contribute",
+    },{
+        type: "input",
+		message: “Please state any test(s) require.”
+        name: "test",
+    },{
+        type: "input",
+		message: “Please state any test(s) require.”
+        name: "test",
+    },{
+        type: "input",
+		message: “Please state any test(s) require.”
+        name: "test",
+    },{
+        type: "input",
+		message: “State your accomplishments.”
+        name: "accomplish",
+    },{
+        type: "input",
+		message: “Please state provide a screenshot (1 of 3).”
+        name: "scriptjs",
+    },{
+        type: "input",
+		message: “Please state provide a screenshot (2 of 3).”
+        name: "fileGnerator",
+    },{
+        type: "input",
+		message: “Please state provide a screenshot (3 of 3).”
+        name: "scriptjs",
+    },{
+        type: "input",
+		message: “Please supply two references.”
+        name: "ref1",
+    },{
+        type: "input",
+		message: “Please state any test(s) require.”
+        name: "ref2”,
+    },{
+        type: "input",
+		message: “Please state your end-goal.”
+        name: "endgoal",
+    }
+];
+
+
+// Function to write to my ReadMe.md file. //
+inquirer.prompt(questions).then(function(response) {
+   console.log(response);
+   
+    var content = fileGenerator(response);
+    console.log(content);
+     fs.writeFile("./ReadMe.md", content, function(err){
+         if (err) throw err
+         console.log("success");
+     });
+} ); 
